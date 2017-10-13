a3907fb86f17f271a93bfdca300c3e8aae9883f8
srirama-Code <sriramazure@gmail.com>
2017-10-13 14:46:32 -0500

    sriram

---
+++ Source/repos/NodejsWebApp1/NodejsWebApp1/server.js
@@ -0,0 +1,14 @@
+"use strict";
+//import http = require('http');
+//var port = process.env.port || 1337
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('jai shriram\n');
+//}).listen(port);
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.listen(8080);
+app.get('/', function (req, res) { res.sendFile(__dirname + '\\' + 'Index.html'); });
+console.log('Server has started now!');
+//# sourceMappingURL=server.js.map
---
+++ Source/repos/NodejsWebApp1/NodejsWebApp1/server.ts
@@ -0,0 +1,13 @@
+﻿//import http = require('http');
+//var port = process.env.port || 1337
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('jai shriram\n');
+//}).listen(port);
+
+import express = require('express');
+var app = express();
+
+app.listen(8080); 
+app.get('/', function (req, res) { res.sendFile(__dirname + '\\' +'Index.html'); });
+console.log('Server has started now!');
